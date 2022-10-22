3a191c4af5a08cc7e3a7bdc346852bbb8863f257
Gulnaz <madam.day@mail.ru>
2022-10-22 11:35:27 +0300

    'first commit variables'

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,22 @@
+'use strict'
 function solveEquation(a, b, c) {
   let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+  let diskriminant = b ** 2 - 4 * a * c;
+  console.log('diskriminant = ' + diskriminant);
+  if (diskriminant < 0) {
+    return false;
+  } else if (diskriminant == 0) {
+      arr = -b / (2 * a);
+      console.log(arr);
+  }
+  if (diskriminant > 0) {
+    let roots = [];
+    roots.push((-b + Math.sqrt(diskriminant)) / (2 * a));
+    roots.push((-b - Math.sqrt(d)) / (2 * a));
+      arr = roots;
+  }
+  return arr;
+  console.log(solveEquation(1, 2, 3));
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
