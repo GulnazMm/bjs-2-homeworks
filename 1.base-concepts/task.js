f03a6095435da9ab34e7051fd46aeab9744b1e9a
Gulnaz <madam.day@mail.ru>
2022-11-02 18:56:59 +0300

    git add task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,22 +1,20 @@
 'use strict'
 function solveEquation(a, b, c) {
-  let arr;
+  let arr = [];
   let diskriminant = b ** 2 - 4 * a * c;
   console.log('diskriminant = ' + diskriminant);
-  if (diskriminant < 0) {
-    return false;
-  } else if (diskriminant == 0) {
-      arr = -b / (2 * a);
+  if  (diskriminant == 0) {
+      arr.push (-b / (2 * a));
       console.log(arr);
   }
   if (diskriminant > 0) {
     let roots = [];
     roots.push((-b + Math.sqrt(diskriminant)) / (2 * a));
-    roots.push((-b - Math.sqrt(d)) / (2 * a));
+    roots.push((-b - Math.sqrt(diskriminant)) / (2 * a));
       arr = roots;
   }
   return arr;
-  console.log(solveEquation(1, 2, 3));
+  console.log(solveEquation(1, 25, 15));
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
