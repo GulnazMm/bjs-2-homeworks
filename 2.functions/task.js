813c818fff4d828a7998be5a40a64e30642438f2
Gulnaz <madam.day@mail.ru>
2022-11-09 17:58:47 +0300

    'first commit variables'

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,45 @@
 // Задание 1
 function getArrayParams(arr) {
   let min, max, sum, avg;
-
-  // Ваш код
-
+  min = Infinity;
+  max = -Infinity;
+  sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max)
+      max = arr[i];
+    if (arr[i] < min)
+      min = arr[i];
+    sum = arr[i] + sum;
+  }
+  avg = Number((sum / arr.length).toFixed(2));
   return { min: min, max: max, avg: avg };
 }
 
 // Задание 2
 function worker(arr) {
   let sum;
-
-  // Ваш код
-
+  for (let i = 0; i < arr.length; i++){
+  sum = sum + arr[i];
+  }
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
   let max;
+  for (let i = 0; i < arrOfArr.length; i++) {
+    let sum = worker(arrOfArr[i]);
+    if (sum > max) {
+      max = sum;
+    }
+  }
 
-  // Ваш кода
-  // for ...
-  
   return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  let result =  getArrayParams(arr);
+ return Math.abs(result.max - result.min);
 }
+makeWork (arrOfArr, worker2);
+
