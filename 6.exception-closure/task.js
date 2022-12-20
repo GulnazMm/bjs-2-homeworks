33a938092add0b17f3e4219e7c870a11eaef5170
Gulnaz <madam.day@mail.ru>
2022-12-20 19:57:01 +0300

    corrected

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,18 +1,18 @@
 function parseCount(number) {
-    Number.parseInt(number);
-    if (Number.isNaN(number)) {
+    const result = Number.parseInt(number);
+    if (Number.isNaN(result)) {
         throw new Error("Невалидное значение");
     }
-    return number;
+    return result;
 }
 
 
-function validateCount(count) {
+function validateCount(number) {
     try {
-        return parseCount(count);
+        return parseCount(number);
     } catch (error) {
-        (console.error("Невалидное значение"));
-    } finally {return count}
+        console.log(error);
+    }
 
 }
 
@@ -21,29 +21,40 @@ class Triangle {
         this.a = a;
         this.b = b;
         this.c = c;
+
+        if (a > b + c || b > a + c || c > a + b) {
+            throw new Error("Треугольник не существует")
+        }
+    }
+    getPerimeter() {
+        let perimeter = this.a + this.b + this.c;
+        return perimeter;
     }
 
-    if (a > b + c || b > a + c || c > a + b); {
-    throw new Error("Треугольник не существует");
-}
-getPerimeter() {
-    console.log(a + b + c);
-}
-
-getArea() {
-let p = (a + b + c) / 2;
-let s = (p * (p - a) * (p - b) * (p - c)) ** 0.5;
-return s.toFixed(3);
-}
-
-function getTriangle (a, b, c) {
-    try{
-        return new Triangle;
-    } catch (error){
-        return new Triangle.getArea.getPerimeterr 
-        (console.log (new Triangle.getArea.getPerimeter + "Ошибка! Треугольник не существует"));
+    getArea() {
+        let p = perimeter / 2;
+        let s = (p * (p - this.a) * (p - this.b) * (p - this.c)) ** 0.5;
+        return + s.toFixed(3);
     }
 }
+
+function getTriangle(a, b, c) {
+    try {
+        let triangle2 = new Triangle(a, b, c);
+    }
+    catch (error) {
+        return triangle2.getArea() {
+            console.log("Ошибка! Треугольник не существует")
+
+        }
+        + triangle2.getPerimeter() {
+            console.log("Ошибка! Треугольник не существует");
+
+        }
+    }
 }
+
+
+
 
 
